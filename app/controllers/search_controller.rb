d8ac706cd99bd152212254f2be5edd8933c6b467
JunePaloma <kborkoski@gmail.com>
2017-10-18 11:16:30 -0600

    Change 'zip' to 'location' in api request url'

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
 
   def index
-    stations_data= StationService.find_by_zip_code(params[:zip])
+    stations_data= StationService.new.find_by_zip_code(params[:zip])
     @stations = StationMaker.new(stations_data).make_stations[:fuel_stations]
   end
 
