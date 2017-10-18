fd7a5dcdeb536657f1ea14871f219c8eb9c49f9b
JunePaloma <kborkoski@gmail.com>
2017-10-18 11:22:00 -0600

    Fix typos in service and interpolate zip code, uninitialized constant error for StationMaker

--- app/services/station_service.rb
+++ app/services/station_service.rb
@@ -2,11 +2,12 @@ class StationService
   def initialize
     @conn = Faraday.new("https://developer.nrel.gov/") do |faraday|
       faraday.adapter Faraday.default_adapter
+    end
   end
 
-def find_by_zip_code(zip_code)
-  response = conn.get("api/alt-fuel-stations/v1/nearest.json?type=ELEC,LPG&limit=10& &location="zip_code"&radius=6&#{ENV["NREL_API_KEY"]}")
-  Json.parse(response.body), sympolize_names: true)
-end
+  def find_by_zip_code(zip_code)
+    response = @conn.get("api/alt-fuel-stations/v1/nearest.json?type=ELEC,LPG&limit=10& &location=#{zip_code}&radius=6&#{ENV["NREL_API_KEY"]}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
 
 end
